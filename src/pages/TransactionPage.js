cb198716a19d3ebb5f4792d11f9a28c39441eea7
maulana dimyati <dimyatimaulana@gmail.com>
2022-12-11 14:31:53 +0700

    add product diajukan !== null

--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -9,33 +9,42 @@ import TransactionPending from '../components/Transaction/TransactionPending';
 import '../styles/TransactionPage.css';
 
 function TransactionPage({ productDiajukan, setProductDiajukan, productDitawar }) {
-  if (productDiajukan.length > 0 && productDitawar.length > 0) {
-    const productDiajukanPending = productDiajukan.filter((product) => product.status === 'pending');
-    const productDiajukanSelesai = productDiajukan.filter((product) => product.status === 'selesai');
-    const productDitawarPending = productDitawar.filter((product) => product.status === 'pending');
+  if (productDiajukan !== null || productDitawar !== null) {
+    if (productDiajukan.length > 0 && productDitawar.length > 0) {
+      const productDiajukanPending = productDiajukan.filter((product) => product.status === 'pending');
+      const productDiajukanSelesai = productDiajukan.filter((product) => product.status === 'selesai');
+      const productDitawarPending = productDitawar.filter((product) => product.status === 'pending');
 
-    return (
-      <div className="transaction-page">
-        <h1>Transaksi</h1>
-        <div className="transaction-page__container">
-          <h2>Pending</h2>
-          <TransactionPending
-            productDiajukan={productDiajukanPending}
-            productDitawar={productDitawarPending}
-          />
-          <h2>Berlangsung</h2>
-          <TransactionTakesPlace
-            productDiajukan={productDiajukanPending}
-            setProductDiajukan={setProductDiajukan}
-            productDitawar={productDitawarPending}
-          />
-          <h2>Selesai</h2>
-          <TransactionCompleted
-            productDiajukan={productDiajukanSelesai}
-          />
+      return (
+        <div className="transaction-page">
+          <h1>Transaksi</h1>
+          <div className="transaction-page__container">
+            <h2>Pending</h2>
+            <TransactionPending
+              productDiajukan={productDiajukanPending}
+              productDitawar={productDitawarPending}
+            />
+            <h2>Berlangsung</h2>
+            <TransactionTakesPlace
+              productDiajukan={productDiajukanPending}
+              setProductDiajukan={setProductDiajukan}
+              productDitawar={productDitawarPending}
+            />
+            <h2>Selesai</h2>
+            <TransactionCompleted
+              productDiajukan={productDiajukanSelesai}
+            />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    if (productDiajukan.length === 0 || productDitawar.length === 0) {
+      return (
+        <div className="transaction-page">
+          <p>Transaksi masih kosong</p>
+        </div>
+      );
+    }
   }
   return (
     <div className="transaction-page">
