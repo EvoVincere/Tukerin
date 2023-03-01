4adf4a95509f90b0b6794f48ae28e9bf5d8fab8c
muazahalwyh <muazahmuazah2001@gmail.com>
2023-03-02 01:33:40 +0700

    menambahkan chatbot

---
+++ src/components/Chatbot.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// class CrispChat extends React.Component {
+//   componentDidMount() {
+//     window.$crisp = [];
+//     window.CRISP_WEBSITE_ID = 'b490c39b-4cc8-4544-a581-22cb62bee69d';
+
+//     const script = document.createElement('script');
+//     script.src = 'https://client.crisp.chat/l.js';
+//     script.async = 1;
+//     document.getElementsByTagName('head')[0].appendChild(script);
+//   }
+
+//   render() {
+//     return null;
+//   }
+// }
+
+// export default CrispChat;
